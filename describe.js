89301ea057e1b23f7e0de16cc4bacdf111f3facc
agentgamer <wdanxna@gmail.com>
2016-04-07 17:40:57 +0800

    init

---
+++ describe.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+'use strict';
+
+var assert = require('assert');
+
+module.exports = (function(){
+	function handles(type) {
+		switch(type) {
+			case 'NaN': {
+				return (function(n) {
+					return isNaN(n);
+				});
+				break;
+			};
+			case 'null': {
+				return (function(n) {
+					return n === null;
+				});
+				break;
+			};
+			case 'array': {
+				return (function(n) {
+					return Array.isArray(n);
+				});
+				break;
+			};
+			default: {
+				return (function(n) {
+					return (typeof n) === type;
+				});
+			}
+		};
+	}
+
+	function iter(tar, src) {
+	    var keys = Object.keys(tar);
+	    assert(keys && keys.length > 0, 'object is empty');
+	    assert(src, 'description is empty');
+	    
+	    for (var key in tar) {
+	    	var srcKey, tarKey;
+	    	srcKey = tarKey = key;
+
+	    	if (src[key] === void 0) {
+	    		srcKey = srcKey + '?';
+			}
+			if (!src[srcKey]) { assert(false, key + 'not described.');}
+
+	        if (typeof src[srcKey] === 'object' && typeof tar[tarKey] === 'object') {
+                iter(tar[tarKey], src[srcKey]);
+	        } else {
+				var variants = [src[srcKey]];
+	    		if (src[srcKey].indexOf('|') !== -1) {
+					variants = src[srcKey].split('|');
+	    		}
+
+	    		var matched = false;
+	    		for (var v = 0; v < variants.length; v++) {
+	        		var check = handles(variants[v]);
+	        		if (check(tar[tarKey])) {
+	        			matched = true;
+	        			break;
+	        		}
+	    		}
+	    		if (!matched) {
+	    			assert(false , key + ' do not match any given description.');
+	    		}
+	        }
+	    }   
+	}
+
+	return (function(obj, desc) {
+	    iter(obj, desc);
+	});
+})()
---
+++ test.js
@@ -0,0 +1,162 @@
+var assert = require('assert');
+
+var desc = require('./describe.js');
+
+describe('describe', function(){
+
+	it('primitive type', function(){
+		var target = {
+			number: 0,
+			string: "this is string",
+			boolean: true,
+			undefinedable: undefined,
+			nullable: null,
+			isnan: 0/0,
+			object: {},
+			array: [1,2,3],
+			function: (function(){})
+		};
+
+		desc(target, {
+			number: 'number',
+			string: 'string',
+			boolean: 'boolean',
+			undefinedable: 'undefined',
+			nullable: 'null',
+			isnan: 'NaN',
+			array: 'array',
+			object: 'object',
+			function: 'function'
+		});
+	});
+
+	it('nesting object', function() {
+		var target = {
+			level1: {
+				level2: {
+					level3: {}
+				}
+			}	
+		};
+
+		desc(target, {
+			level1: {
+				level2: {
+					level3: 'object'
+				}
+			}
+		});
+	});
+
+	describe('#optional value', function(){
+		it('exists', function(){
+			var target = {
+				positive: 1,
+				optional: 2
+			};
+
+			desc(target, {
+				positive: 'number',
+				'optional?': 'number'
+			});
+
+		});
+
+		it('not exists', function() {
+			var target = {
+				positive: 1
+			};
+
+			desc(target, {
+				positive: 'number',
+				'optional?': 'number'
+			});
+		});
+
+		it('nested', function(){
+			var target = {
+				positive: 1,
+				optional: {
+					say: 'yes'
+				}
+			};
+
+			desc(target, {
+				positive: 'number',
+				'optional?': {
+					say: 'string'
+				}
+			});
+		});
+
+		it('nested2', function(){
+			var target = {
+				positive: 1,
+			};
+
+			desc(target, {
+				positive: 'number',
+				'optional?': {
+					say: 'string'
+				}
+			});
+		});
+	});
+
+	describe('#variant', function(){
+		it('one choice', function(){
+			var target = {
+				maybe: {
+					say: 'yes'
+				}
+			};
+
+			desc(target, {
+				maybe: 'object|undefined'
+			});
+		});
+
+		it('another choice', function(){
+			var target = {
+				maybe: void 0
+			};
+
+			desc(target, {
+				maybe: 'object|undefined'
+			});
+		});
+
+		describe('##multiple choice', function() {
+			var descriptor = {
+				'solid?': 'number|string',
+				maybe: 'undefined|number|object|string'
+			};
+
+			it('1', function(){
+				desc({
+					solid: 1,
+					maybe: void 0
+				},descriptor);
+			});
+
+			it('2', function(){
+				desc({
+					solid: 'hey',
+					maybe: 1
+				}, descriptor);
+			});
+
+			it('3', function(){
+				desc({
+					maybe: {}
+				}, descriptor);
+			});
+
+			it('4', function(){
+				desc({
+					maybe: 'hey'
+				}, descriptor);
+			});
+		});
+	});
+});
